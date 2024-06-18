589b0160c4307e12666d5711255ea74421eaafbd
20100Lad <79887916+20100Lad@users.noreply.github.com>
2024-06-17 23:22:13 -0400

    line renderer done
    prefab et switch de particulespace

--- Assets/02-Script/ParticlesPlexus.cs
+++ Assets/02-Script/ParticlesPlexus.cs
@@ -6,17 +6,22 @@ public class Plexus : MonoBehaviour
 {
     public float maxDistance = 1.0f;
 
-    new ParticleSystem particleSystem; 
+    new ParticleSystem particleSystem;
     ParticleSystem.Particle[] particles;
 
     ParticleSystem.MainModule particlesSystemMainModule;
 
     public LineRenderer lineRendererTemplate;
     List<LineRenderer> lineRenderers = new List<LineRenderer>();
+
+    Transform _transform;
     void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
         particlesSystemMainModule = particleSystem.main;
+
+        _transform = transform;
+
     }
 
     void LateUpdate()
@@ -25,26 +30,80 @@ public class Plexus : MonoBehaviour
 
         float maxDistanceSqr = maxDistance * maxDistance;
 
-        if (particles == null || particles.Length <maxParticles)
+        int lrIndex = 0;
+        int lineRendererCount = lineRenderers.Count;
+
+
+                if (particles == null || particles.Length < maxParticles)
+                {
+                    particles = new ParticleSystem.Particle[maxParticles];
+                }
+
+                particleSystem.GetParticles(particles);
+                int particleCount = particleSystem.particleCount;
+
+        switch (particlesSystemMainModule.simulationSpace)
         {
-            particles = new ParticleSystem.Particle[maxParticles];
+            case ParticleSystemSimulationSpace.Local:
+                {
+                    _transform = transform;
+                    lineRendererTemplate.useWorldSpace = false;
+                    break;
+                }
+            case ParticleSystemSimulationSpace.Custom:
+                {
+                    _transform = particlesSystemMainModule.customSimulationSpace;
+                    lineRendererTemplate.useWorldSpace = false;
+                    break;
+                }
+            case ParticleSystemSimulationSpace.World:
+                {
+                    _transform = transform;
+                    lineRendererTemplate.useWorldSpace = true;
+                    break;
+                }
+            default:
+                {
+                    throw new System.NotSupportedException(
+                        string.Format("Unsupported simulation space '{0}'.", System.Enum.GetName(typeof(ParticleSystemSimulationSpace), particlesSystemMainModule.simulationSpace)));
+                }
         }
+                for (int i = 0; i < particleCount; i++)
+                {
+                    Vector3 p1_position = particles[i].position;
 
-        particleSystem.GetParticles(particles);
-        int particleCount = particleSystem.particleCount;
+                    for (int j = i + 1; j < particleCount; j++)
+                    {
+                        Vector3 p2_position = particles[j].position;
+                        float distanceSqr = Vector3.SqrMagnitude(p1_position - p2_position);
 
-        for (int i = 0; i < particleCount; i++) 
-        {
-            Vector3 p1_position = particles[i].position;
+                        if (distanceSqr <= maxDistanceSqr)
+                        {
+                            LineRenderer lr;
 
-            for (int j = i + 1; j < particleCount; j++) 
-            {
-                Vector3 p2_position = particles[i].position;
-                float distanceSqr = Vector3.SqrMagnitude(p1_position - p2_position);
+                            if (lrIndex == lineRendererCount)
+                            {
+                                lr = Instantiate(lineRendererTemplate, _transform, false);
+                                lineRenderers.Add(lr);
 
-            }
+                                lineRendererCount++;
+                            }
+                            lr = lineRenderers[lrIndex];
+
+                            lr.enabled = true;
+
+                            lr.SetPosition(0, p1_position);
+                            lr.SetPosition(1, p2_position);
+
+                            lrIndex++;
+                        }
+                    }
+                }
+
+                for (int i = lrIndex; i < lineRendererCount; i++)
+
+                {
+                    lineRenderers[i].enabled = false;
+                }
         }
-
-
-    }
 }
